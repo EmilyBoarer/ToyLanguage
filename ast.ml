a86a43103f4c29e084007e9a38a36dbab726eeb1
EmilyBoarer <prog@emily.boarer.uk>
2023-06-12 22:56:37 +0100

    type checking: allow alone identifiers to return their value

--- ast.ml
+++ ast.ml
@@ -42,7 +42,6 @@ let rec vt_check = function (* helper function: determine which type is associat
     | s1, VT(s2, t)::tail -> if s1=s2 then t else vt_check (s1, tail)
     | _, [] -> []
 
-
 let rec intersection2 = function (* helper helper function *)
     | l1, h1::t1, h2::t2 -> let r = intersection2 (l1, t1, h2::t2) in
                             (if h1=h2 then h1::r else r )
@@ -51,22 +50,36 @@ let rec intersection2 = function (* helper helper function *)
 let rec intersection = function (* helper function: intersection of two lists (of types)*)
     l1, l2 -> intersection2 (l1, l1, l2)
 
+let rec types_of = function (* helper function: return list of all types of identifier with name str *)
+    | str, VT(s, ts)::t -> if str = s then ts else types_of (str, t)
+    | _, [] -> []
+
+let rec contains_type = function (* helper function: returns true iff list of types contains given type *)
+    | ty, h::t -> if ty = h then true else contains_type (ty, t)
+    | _, [] -> false
+
 
 let rec type_check = function (* ast node, variable_types -> acceptable_types list*)
     | DECLARE (ast1, ast2), vt -> (* TODO check not already declared under same name! => ct_check -> invalid *)
         (match type_check (ast2, vt) with
             | [], _ -> [], vt
             | checked_var_types, _ ->
-                (match (type_check (ast1, vt)), ast1 with
-                    | (IDENT_T::[],_), IDENT(str) -> [UNIT_T], (VT(str, checked_var_types))::vt
-                    | _ -> [], vt))
+                (match type_check (ast1, vt) with
+                    tl, _ -> (if contains_type (IDENT_T, tl) then
+                        (match ast1 with
+                            | IDENT(str) -> [UNIT_T], (VT(str, checked_var_types))::vt
+                            | _ -> [], vt)
+                        else [], vt)))
 
     | ASSIGN (ast1, ast2), vt -> (* Assign should return the value being assigned *)
-        (match (type_check (ast1, vt)), ast1 with (* check the that identifier has that type associated with it at this point in the program *)
-            | (IDENT_T::[],_), IDENT(str) ->
-                let t1, _ = type_check (ast2, vt) in
-                intersection ((vt_check (str, vt)),t1)
-            | _, _ -> []), vt
+        (match type_check (ast1, vt) with
+            | tl, _ -> (if contains_type (IDENT_T, tl) then
+                (match ast1 with (* check the that identifier has that type associated with it at this point in the program *)
+                    | IDENT(str) ->
+                        let t1, _ = type_check (ast2, vt) in
+                        intersection ((vt_check (str, vt)),t1)
+                    | _ -> []), vt
+                else [], vt))
 
     | SEQ (h::[]), vt -> type_check (h, vt) (* Seq should return (return of the last item in the sequence) *)
     | SEQ (h::t), vt ->
@@ -75,11 +88,17 @@ let rec type_check = function (* ast node, variable_types -> acceptable_types li
             | _, vt2 -> type_check (SEQ(t), vt2))
 
     | INT (_), vt -> [I32_T; U32_T], vt (* allow for multiple different integer representations. TODO check that each int is representable under the given range e.g. disallowing u32 for negative numbers *)
-    | IDENT (_), vt -> [IDENT_T], vt
+    | IDENT (str), vt -> let ts = types_of (str, vt) in
+                         IDENT_T::ts, vt
     | TYPE_IDENT (t), vt -> [t], vt
     | _, vt -> [], vt
 
 
-let rec simplify_then_type_check = function x -> type_check ((simplify_ast x), [VT("x", [I32_T])])
+let simplify_then_type_check = function x -> type_check ((simplify_ast x), [])
 
 (*TODO check each variable is assigned only 1 type in the end! - or at least pick one!*)
+
+let run = simplify_then_type_check ( SEQ([
+                                            LET(IDENT("x"), TYPE_IDENT(U32_T), INT(7));
+                                            IDENT("x")
+                                     ]) )
